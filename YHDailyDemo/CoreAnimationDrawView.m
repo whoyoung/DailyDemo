f5e7528a110f68fa49dce8aefce1035dfc93e829
杨虎 <yanghu@xiaoshouyi.com>
2018-01-26 14:05:39 +0800

    绘制x轴文字

--- YHDailyDemo/CoreAnimationDrawView.m
+++ YHDailyDemo/CoreAnimationDrawView.m
@@ -19,6 +19,7 @@
     [super layoutSubviews];
     if(!_axisLayer) {
         [self.layer addSublayer:self.axisLayer];
+        [self addTextLayer];
     }
 }
 
@@ -26,7 +27,7 @@
     if (!_axisLayer) {
         CAShapeLayer *frameLayer = [CAShapeLayer layer];
         CGFloat frameX = 50, frameY = 10;
-        CGFloat frameW = self.bounds.size.width-frameX*2, frameH = self.bounds.size.height-frameY*2;
+        CGFloat frameW = self.bounds.size.width-frameX*2, frameH = self.bounds.size.height-frameY-20;
         CGRect frameRect = CGRectMake(frameX, frameY, frameW, frameH);
         
         UIBezierPath *framePath = [UIBezierPath bezierPathWithRect:frameRect];
@@ -55,5 +56,32 @@
         _axisLayer = frameLayer;
     }
     return _axisLayer;
+}
+- (void)addTextLayer {
+    CGFloat axisX = 50;
+    CGFloat axisMaxY = self.bounds.size.height-15;
+    CGFloat axisUnitW = (self.bounds.size.width-axisX*2)/6;
+    for (NSUInteger i=0; i<6; i++) {
+        CGRect textFrame = CGRectMake(axisX+axisUnitW/2.0 + axisUnitW*i, axisMaxY, axisUnitW, 15);
+        CATextLayer *text = [self getTextLayerWithString:[NSString stringWithFormat:@"x%ld",i] textColor:[UIColor blackColor] fontSize:12 backgroundColor:[UIColor clearColor] frame:textFrame];
+        [self.layer addSublayer:text];
+    }
+}
+
+- (CATextLayer *)getTextLayerWithString:(NSString *)text
+                              textColor:(UIColor *)textColor
+                               fontSize:(NSInteger)fontSize
+                        backgroundColor:(UIColor *)bgColor
+                                  frame:(CGRect)frame {
+    CATextLayer *textLayer = [CATextLayer layer];
+    textLayer.frame = frame;
+    textLayer.string = text;
+    textLayer.fontSize = fontSize;
+    textLayer.foregroundColor = textColor.CGColor;
+    textLayer.backgroundColor = bgColor.CGColor;
+    textLayer.alignmentMode = kCAAlignmentCenter;
+    //设置分辨率
+    textLayer.contentsScale = [UIScreen mainScreen].scale;
+    return textLayer;
 }
 @end
