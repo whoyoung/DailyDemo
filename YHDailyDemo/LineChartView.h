05014155099c314a29faad6685f4b243f51a77e8
杨虎 <yanghu@xiaoshouyi.com>
2018-01-27 14:41:36 +0800

    初始化

---
+++ YHDailyDemo/LineChartView.h
@@ -0,0 +1,13 @@
+//
+//  LineChartView.h
+//  YHDailyDemo
+//
+//  Created by 杨虎 on 2018/1/26.
+//  Copyright © 2018年 杨虎. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LineChartView : UIView
+
+@end
---
+++ YHDailyDemo/LineChartView.m
@@ -0,0 +1,112 @@
+//
+//  LineChartView.m
+//  YHDailyDemo
+//
+//  Created by 杨虎 on 2018/1/26.
+//  Copyright © 2018年 杨虎. All rights reserved.
+//
+
+static const float TopEdge = 10;
+static const float LeftEdge = 50;
+static const float RightEdge = 10;
+static const float BottomEdge = 20;
+static const float minItemWidth = 20;
+#define LineChartWidth (self.bounds.size.width-LeftEdge-RightEdge)
+#define LineChartHeight (self.bounds.size.height-TopEdge-BottomEdge)
+
+#import "LineChartView.h"
+@interface LineChartView()<UIScrollViewDelegate>
+@property (nonatomic, weak) UIScrollView *gestureScroll;
+@property (nonatomic, strong) NSMutableArray *xAxisArray;
+@property (nonatomic, strong) NSMutableArray *yValueArray;
+
+@property (nonatomic, assign) NSUInteger beginIndex;
+@property (nonatomic, assign) NSUInteger endIndex;
+@property (nonatomic, assign) CGFloat itemW;
+@property (nonatomic, assign) CGFloat maxYValue;
+@property (nonatomic, assign) CGFloat minYValue;
+@end
+
+@implementation LineChartView
+
+- (void)layoutSubviews {
+    [super layoutSubviews];
+    
+    [self addGestureScroll];
+    [self findMaxAndMinValue:_beginIndex rightIndex:_endIndex];
+    
+}
+
+- (void)addGestureScroll {
+    if (!_gestureScroll) {
+        UIScrollView *scroll = [[UIScrollView alloc] initWithFrame:self.bounds];
+        scroll.showsVerticalScrollIndicator = NO;
+        scroll.showsHorizontalScrollIndicator = NO;
+        scroll.minimumZoomScale = 1.0;
+        scroll.maximumZoomScale = 1.0;
+        scroll.bounces = NO;
+        scroll.delegate = self;
+        scroll.backgroundColor = [UIColor blueColor];
+        _gestureScroll = scroll;
+        [self addSubview:scroll];
+    }
+}
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView {
+    
+}
+
+- (void)findBeginAndEndIndex {
+    CGPoint offset = self.gestureScroll.contentOffset;
+    self.beginIndex = floor(offset.x/self.itemW);
+    self.endIndex = ceil((offset.x+LineChartWidth)/self.itemW);
+    if (self.endIndex > self.yValueArray.count - 1) {
+        self.endIndex = self.yValueArray.count - 1;
+    }
+}
+
+- (void)findMaxAndMinYValue {
+    
+}
+- (void)findMaxAndMinValue:(NSUInteger)leftIndex rightIndex:(NSUInteger)rightIndex {
+    if (leftIndex == rightIndex) {
+        self.minYValue = MIN([self.yValueArray[leftIndex] floatValue], self.minYValue);
+        self.maxYValue = MAX([self.yValueArray[leftIndex] floatValue], self.maxYValue);
+        return;
+    } else if(leftIndex == rightIndex-1) {
+        if ([self.yValueArray[leftIndex] floatValue] < [self.yValueArray[rightIndex] floatValue]) {
+            self.minYValue = MIN([self.yValueArray[leftIndex] floatValue], self.minYValue);
+            self.maxYValue = MAX([self.yValueArray[rightIndex] floatValue], self.maxYValue);
+            return;
+        } else {
+            self.minYValue = MIN([self.yValueArray[rightIndex] floatValue], self.minYValue);
+            self.maxYValue = MAX([self.yValueArray[leftIndex] floatValue], self.maxYValue);
+            return;
+        }
+    }
+    NSUInteger mid = (leftIndex + rightIndex)/2;
+    [self findMaxAndMinValue:leftIndex rightIndex:mid];
+    [self findMaxAndMinValue:mid + 1 rightIndex:rightIndex];
+}
+- (void)redraw {
+    
+}
+
+- (NSMutableArray *)xAxisArray {
+    if (!_xAxisArray) {
+        _xAxisArray = [NSMutableArray arrayWithObjects:@"Mon",@"",@"Wed",@"Thu",@"Fri",@"Sat",@"Sun", nil];
+    }
+    return _xAxisArray;
+}
+- (NSMutableArray *)yValueArray {
+    if (!_yValueArray) {
+        _yValueArray = [NSMutableArray arrayWithObjects:@50,@20,@70,@30,@11,@59,@199, nil];
+    }
+    return _yValueArray;
+}
+- (CGFloat)itemW {
+    if (_itemW == 0) {
+        _itemW = LineChartWidth/self.yValueArray.count > minItemWidth ? (LineChartWidth/self.yValueArray.count) : minItemWidth;
+    }
+    return _itemW;
+}
+@end
