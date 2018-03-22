e1b0bc4dd9524be8bf833982184803bc77566e8b
yanghu <huyang@mail.bistu.edu.cn>
2018-03-22 23:54:09 +0800

    semaphore lock

--- YHDailyDemo/LockDemo/LockDemoViewController.m
+++ YHDailyDemo/LockDemo/LockDemoViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "LockDemoViewController.h"
+#import <pthread.h>
 
 @interface LockDemoViewController ()
 @property (nonatomic, strong) NSArray *datas;
@@ -16,7 +17,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    _datas = @[@"recursiveLock"];
+    _datas = @[@"recursiveLock",@"pthreadRecursiveLock",@"semaphoreLock"];
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
@@ -57,5 +58,59 @@
         };
             RecursiveLock(3);
     });
+}
+
+/**
+ pthread
+ */
+// 摘录于YYKit
+static inline void pthread_mutex_init_recursive(pthread_mutex_t *mutex, bool recursive) {
+#define YYMUTEX_ASSERT_ON_ERROR(x_) do { \
+__unused volatile int res = (x_); \
+assert(res == 0); \
+} while (0)
+    assert(mutex != NULL);
+    if (!recursive) {
+        //普通锁
+        YYMUTEX_ASSERT_ON_ERROR(pthread_mutex_init(mutex, NULL));
+    } else {
+        //递归锁
+        pthread_mutexattr_t attr;
+        YYMUTEX_ASSERT_ON_ERROR(pthread_mutexattr_init (&attr));
+        YYMUTEX_ASSERT_ON_ERROR(pthread_mutexattr_settype (&attr, PTHREAD_MUTEX_RECURSIVE));
+        YYMUTEX_ASSERT_ON_ERROR(pthread_mutex_init (mutex, &attr));
+        YYMUTEX_ASSERT_ON_ERROR(pthread_mutexattr_destroy (&attr));
+    }
+#undef YYMUTEX_ASSERT_ON_ERROR
+}
+
+- (void)pthreadRecursiveLock {
+    __block pthread_mutex_t lock;
+    pthread_mutex_init_recursive(&lock,true);
+    dispatch_async(dispatch_get_global_queue(0, 0), ^{
+        static void (^RecursiveLock)(int);
+        RecursiveLock = ^(int value){
+            pthread_mutex_lock(&lock);
+            if (value > 0) {
+                NSLog(@"%d",value);
+                sleep(1);
+                RecursiveLock(--value);
+            }
+            pthread_mutex_unlock(&lock);
+            NSLog(@"after Lock");
+            
+        };
+        RecursiveLock(3);
+    });
+}
+
+- (void)semaphoreLock {
+    dispatch_semaphore_t sema = dispatch_semaphore_create(1);
+    for (int i=3; i>0; i--) {
+        dispatch_semaphore_wait(sema, DISPATCH_TIME_FOREVER);
+        NSLog(@"%d",i);
+        sleep(1);
+        dispatch_semaphore_signal(sema);
+    }
 }
 @end
