c283502b799ef7cfa44886283b8de3253cd023bf
yanghu <yanghu@keep.com>
2019-08-16 11:17:09 +0800

    dictionary to array

--- YHDailyDemo/BrainStroming/BrainStromingViewController.m
+++ YHDailyDemo/BrainStroming/BrainStromingViewController.m
@@ -17,7 +17,40 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
 
-    [self decoderUTF8String];
+//    [self decoderUTF8String];
+//    [self decodeBase64Str];
+//    [self arrayToDict];
+    [self sendMessageToNil];
+//    NSUInteger timeStamp = @([NSDate date].timeIntervalSince1970).unsignedIntValue * 1000;
+//    NSArray *array = @[@{@"timestamp":@"timeStamp"}];
+//    NSDictionary *dict = @{@"timestamp":@"timeStamp"};
+    NSDictionary *dict = [NSDictionary dictionaryWithObjectsAndKeys:@"stamp",@"key", nil];
+    NSArray *array = @[dict];
+    NSLog(@"%@",array);
+}
+
+- (void)sendMessageToNil {
+    NSDictionary *dict = @{};
+    NSDictionary *subDict = [dict objectForKey:@"test"];
+    NSUInteger number = [[subDict objectForKey:@"number"] integerValue];
+    NSLog(@"number = %ld",number);
+    
+    NSDictionary *indexDict = dict[@"test"];
+    NSUInteger indexNumber = [indexDict[@"number"] integerValue];
+    NSLog(@"indexNumber = %ld",indexNumber);
+}
+
+- (void)arrayToDict {
+    NSArray *arr = @[@"a",@"b",@"c"];
+    NSDictionary *dict = (NSDictionary *)arr;
+    NSLog(@"dict === %@",dict);
+}
+
+- (void)decodeBase64Str {
+    NSString *str = @"WwogIDAsCiAgMCwKICAyNTQsCiAgMjU0LAogIDI1NCwKICAyNTQsCiAgMjU0LAogIDI1NCwKICAyNTQsCiAgMjU0LAogIDI1NCwKICAyNTQsCiAgMjU0LAogIDI1NCwKICAyNTQsCiAgMjU0LAogIDI1NCwKICAyNTQsCiAgMjU0LAogIDI1NCwKICAyNTQsCiAgMjU0LAogIDI1NCwKICAyNTQsCiAgMjU0LAogIDI1NCwKICAyNTQsCiAgMjU0LAogIDI1NCwKICAyNTQsCiAgMjU0LAogIDI1NCwKICAyNTQsCiAgMjU0LAogIDI1NCwKICAyNTQsCiAgMjU0LAogIDI1NCwKICAyNTQsCiAgMjU0LAogIDI1NCwKICAyNTQsCiAgMjU0LAogIDI1NCwKICAyNTQsCiAgMjU0LAogIDI1NCwKICAyNTQsCiAgMjU0LAogIDI1NCwKICAyNTQsCiAgMjU0LAogIDI1NCwKICAyNTQsCiAgMjU0LAogIDI1NCwKICAyNTQsCiAgMjU0LAogIDI1NCwKICAyNTQsCiAgMjU0LAogIDI1NCwKICAyNTQsCiAgMjU0LAogIDI1NCwKICAyNTQsCiAgMjU0LAogIDI1NCwKICAyNTQsCiAgMjU0LAogIDI1NCwKICAyNTQsCiAgMjU0LAogIDI1NCwKICAyNTQsCiAgMjU0LAogIDI1NCwKICAyNTQsCiAgMjU0LAogIDI1NCwKICAyNTQsCiAgMjU0LAogIDI1NCwKICAwLAogIDAsCiAgMCwKICAwLAogIDAsCiAgMCwKICAwLAogIDAsCiAgMCwKICAwLAogIDAsCiAgMCwKICAwLAogIDAsCiAgMCwKICAwLAogIDAsCiAgMCwKICAwLAogIDAsCiAgMCwKICAwLAogIDAsCiAgMCwKICAwLAogIDAsCiAgMCwKICAwLAogIDAsCiAgMCwKICAwLAogIDAsCiAgMCwKICAwLAogIDAsCiAgMCwKICAwLAogIDAsCiAgMCwKICAwLAogIDAsCiAgMCwKICAwLAogIDAsCiAgMCwKICAwLAogIDAsCiAgMCwKICAwLAogIDAsCiAgMCwKICAwLAogIDAsCiAgMCwKICAwLAogIDAsCiAgMCwKICAwLAogIDAsCiAgMCwKICAwLAogIDAsCiAgMCwKICAwLAogIDAsCiAgMCwKICAwLAogIDAsCiAgMCwKICAwLAogIDAsCiAgMCwKICAwLAogIDAsCiAgMCwKICAwLAogIDAsCiAgMCwKICAwLAogIDAsCiAgMCwKICAwLAogIDAsCiAgMCwKICAwLAogIDAsCiAgMCwKICAwLAogIDAsCiAgMCwKICAwLAogIDAsCiAgMCwKICAwLAogIDAsCiAgMCwKICAwLAogIDAsCiAgMCwKICAwLAogIDAsCiAgMCwKICAwLAogIDAsCiAgMCwKICAwLAogIDAsCiAgMCwKICAwLAogIDAsCiAgMCwKICAwLAogIDAsCiAgMCwKICAwLAogIDAsCiAgMCwKICAwLAogIDAsCiAgMCwKICAwLAogIDAsCiAgMCwKICAwLAogIDAsCiAgMCwKICAwLAogIDAsCiAgMCwKICAwLAogIDAsCiAgMCwKICAwLAogIDAsCiAgMCwKICAwLAogIDAsCiAgMCwKICAwLAogIDAsCiAgMCwKICAwLAogIDAsCiAgMCwKICAwLAogIDAsCiAgMCwKICAwLAogIDAsCiAgMCwKICAwLAogIDAsCiAgMCwKICAwLAogIDAsCiAgMCwKICAwLAogIDAsCiAgMCwKICAwLAogIDAsCiAgMCwKICAwLAogIDAsCiAgMCwKICAwLAogIDAsCiAgMCwKICAwLAogIDAsCiAgMCwKICAwLAogIDAsCiAgMCwKICAwLAogIDAsCiAgMCwKICAwLAogIDAsCiAgMCwKICAwLAogIDAsCiAgMCwKICAwLAogIDAsCiAgMCwKICAwLAogIDAsCiAgMCwKICAwLAogIDAsCiAgMCwKICAwLAogIDAsCiAgMCwKICAwLAogIDAsCiAgMCwKICAwLAogIDAsCiAgMCwKICAwLAogIDAsCiAgMCwKICAwCl0=";
+    NSData *data = [[NSData alloc] initWithBase64EncodedString:str options:NSDataBase64DecodingIgnoreUnknownCharacters];
+    NSArray *hrs = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:nil];
+    NSLog(@"%@",[hrs componentsJoinedByString:@","]);
 }
 
 - (void)decoderUTF8String {
