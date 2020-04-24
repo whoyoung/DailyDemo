987c1c7b6321065aa015af45f1226f42c5c446ca
yanghu <yanghu@zhihu.com>
2020-04-24 11:57:44 +0800

    设置 protocol 作为 dictionary 的 key

--- YHDailyDemo/MLeaksFinderDemo/MLeaksFinderDemoViewController.m
+++ YHDailyDemo/MLeaksFinderDemo/MLeaksFinderDemoViewController.m
@@ -9,9 +9,13 @@
 
 #import "MLeaksFinderDemoViewController.h"
 #import "LeakView.h"
+#import "TestDemoProtoc.h"
 @interface MLeaksFinderDemoViewController ()
 @property (nonatomic, strong) LeakView *leakV;
 @property (nonatomic, strong) UIColor *color;
+
+@property (nonatomic, strong) NSMapTable *dict;
+
 @end
 
 @implementation MLeaksFinderDemoViewController
@@ -21,9 +25,11 @@
     self.view.backgroundColor = [UIColor whiteColor];
     _leakV = [[LeakView alloc] initWithFrame:CGRectMake(0, 100, [UIScreen mainScreen].bounds.size.width, 100)];
     _leakV.block = ^{
-        self.view.backgroundColor = [UIColor blueColor];
+//        self.view.backgroundColor = [UIColor blueColor];
     };
     [self.view addSubview:_leakV];
+    self.dict = [NSMapTable strongToWeakObjectsMapTable];
+    [self.dict setObject:@"1" forKey:@protocol(TestDemoProtoc)];
 }
 
 
---
+++ YHDailyDemo/MLeaksFinderDemo/TestDemoProtoc.h
@@ -0,0 +1,19 @@
+//
+//  TestDemoProtoc.h
+//  YHDailyDemo
+//
+//  Created by 杨虎 on 2020/1/10.
+//  Copyright © 2020 杨虎. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@protocol TestDemoProtoc <NSObject>
+
+- (void)test;
+
+@end
+
+NS_ASSUME_NONNULL_END
