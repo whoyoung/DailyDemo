5eb1e45f1b147fb73130b03838e132e5faf15559
yanghu <mr_yangh@163.com>
2021-08-26 04:04:03 +0800

    sourcekitten demo

---
+++ YHDailyDemo/SourcekittenDemo/SourcekittenDemoViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SourcekittenDemoViewController.swift
+//  YHDailyDemo
+//
+//  Created by young on 2021/8/26.
+//  Copyright © 2021 杨虎. All rights reserved.
+//
+
+import UIKit
+
+class SourcekittenDemoViewController: UIViewController {
+    private var testVar: String?
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.lightGray
+        testVar = "i am testVar"
+    }
+}
